2cf66b162c1e20994460fef6c908b32105f6dedc
VLEEUWERO <156658682+VLEEUWERO@users.noreply.github.com>
2024-01-18 15:27:51 +0100

    Update streamlit_app.py
    app v2 #updated using discussion posted 22 days ago by 005VI000000K1PlYAK

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,60 +2,43 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 
-# #initial test
-# my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-# my_data_row = my_cur.fetchone()
-# st.text("Hello from Snowflake:")
-# st.text(my_data_row)
+#updated using discussion posted 22 days ago by 005VI000000K1PlYAK
 
+st.title("Zena's Amazing Athleisure Catalog")
 
-    #st.header with "Zena's Amazing Athleisure Catalog"
-st.title('Zena\'s Amazing Athleisure Catalog')
-
-    #Pick a sweatsuit color or style:
-# connect to snowflake
+# Connect to Snowflake
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-# run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog_for_website")
+
+# Run a Snowflake query
+my_cur.execute("SELECT color_or_style FROM catalog_for_website")
 my_catalog = my_cur.fetchall()
-# put the dafta into a dataframe
-df = pd.DataFrame(my_catalog)
-# # temp write the dataframe to the page so I Can see what I am working with
-# st.text('write the dataframe to the page so I Can see what I am working with')
-# st.write(df)
-# put the first column into a list
-color_list = df[0].values.tolist()
-print(color_list)
 
-    #st. single pick Color/Style
+# Put the data into a DataFrame
+df = pd.DataFrame(my_catalog, columns=['color_or_style'])
+
+# Put the first column into a list
+color_list = df['color_or_style'].tolist()
+
 # Let's put a pick list here so they can pick the color
-option = st.selectbox('Pick a sweatsuit color or style:', list(color_list))
+option = st.selectbox('Pick a sweatsuit color or style:', color_list)
 
-
-    #render image from URL
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
-# use the option selected to go back and get all the info from the database
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
+
+# Use the option selected to go back and get all the info from the database
+query = """
+SELECT direct_url, price, size_list, upsell_product_desc 
+FROM catalog_for_website 
+WHERE color_or_style = '{}';
+""".format(option)
+my_cur.execute(query)
 df2 = my_cur.fetchone()
-st.image(
-df2[0],
-width=400,
-    #Our warm, comfortable, {f Yellow sweatsuit}!
-caption= product_caption
-)
 
-    #Price: 65.00
-st.write('Price: ', df2[1])
-
-    #Sizes Available: Child L | Child M | Child S ..... Women's XXL
-st.write('Sizes Available: ',df2[2])
-
-#Consider white/grey/black
-#OR
-#BONUS: 1:Yellow Headband & 2 Pink Wristbands
-st.write(df2[3])
-
+if df2:
+    st.image(df2[0], width=400, caption=product_caption)
+    st.write('Price: ', df2[1])
+    st.write('Sizes Available: ', df2[2])
+    st.write(df2[3])
+else:
+    st.write("No details found for the selected option.")

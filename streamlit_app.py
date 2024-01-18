7d9cbd037f21c7aed551e7bdc3b122b10aa79d67
VLEEUWERO <156658682+VLEEUWERO@users.noreply.github.com>
2024-01-18 15:10:56 +0100

    Update streamlit_app.py
    cut out code to check role snowflake is correct

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import snowflake.connector
+import pandas as pd
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -8,15 +9,3 @@ CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 st.text("Hello from Snowflake:")
 st.text(my_data_row)
-
-#st.header with "Zena's Amazing Athleisure Catalog"
-#Pick a sweatsuit solor or style:
-#st. single pick Color/Style
-#render image from URL
-#Our warm, comfortable, {f Yellow sweatsuit}!
-#Price: 65.00
-#Sizes Available: Child L | Child M | Child S ..... Women's XXL
-#
-#Consider white/grey/black
-#OR
-#BONUS: 1:Yellow Headband & 2 Pink Wristbands

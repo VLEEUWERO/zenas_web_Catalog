c6a89382a54a962298a7cd607ee0bba2a655c4e8
VLEEUWERO <156658682+VLEEUWERO@users.noreply.github.com>
2024-01-18 14:57:06 +0100

    Create streamlit_app.py
    created the main .py file with a streamlit import and the outline of the app written down line be line on what it should do

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+#st.header with "Zena's Amazing Athleisure Catalog"
+#Pick a sweatsuit solor or style:
+#st. single pick Color/Style
+#render image from URL
+#Our warm, comfortable, {f Yellow sweatsuit}!
+#Price: 65.00
+#Sizes Available: Child L | Child M | Child S ..... Women's XXL
+#
+#Consider white/grey/black
+#OR
+#BONUS: 1:Yellow Headband & 2 Pink Wristbands

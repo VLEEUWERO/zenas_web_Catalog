a635529245f5b786c6306a9120a80edce9838459
VLEEUWERO <156658682+VLEEUWERO@users.noreply.github.com>
2024-01-18 15:17:51 +0100

    Update streamlit_app.py
    streamlist as st bugfixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,11 @@ import pandas as pd
 
 
     #st.header with "Zena's Amazing Athleisure Catalog"
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
+st.title('Zena\'s Amazing Athleisure Catalog')
 
     #Pick a sweatsuit color or style:
 # connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
@@ -41,7 +41,7 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
 df2 = my_cur.fetchone()
-streamlit.image(
+st.image(
 df2[0],
 width=400,
     #Our warm, comfortable, {f Yellow sweatsuit}!
@@ -49,13 +49,13 @@ caption= product_caption
 )
 
     #Price: 65.00
-streamlit.write('Price: ', df2[1])
+st.write('Price: ', df2[1])
 
     #Sizes Available: Child L | Child M | Child S ..... Women's XXL
-streamlit.write('Sizes Available: ',df2[2])
+st.write('Sizes Available: ',df2[2])
 
 #Consider white/grey/black
 #OR
 #BONUS: 1:Yellow Headband & 2 Pink Wristbands
-streamlit.write(df2[3])
+st.write(df2[3])
 
